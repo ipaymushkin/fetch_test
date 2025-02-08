b065fea069195cd0a53d86c10219e3048481c158
ipaymushkin <57440215+ipaymushkin@users.noreply.github.com>
2025-02-08 10:47:10 +0300

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,10 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
   const apiKey = 'f2ee48b1843341f083762954250802';
+
+  try {
+    const response = await fetch('http://api.weatherapi.com/v1/current.json?key=f2ee48b1843341f083762954250802&q=London&aqi=no');
+    console.log(response);
+  } catch (error) {
+    console.log(error)
+  }
 });
