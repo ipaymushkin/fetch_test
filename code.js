bacd49bd83dadeee76f47dd529592481e8b8390a
ipaymushkin <57440215+ipaymushkin@users.noreply.github.com>
2025-02-08 11:20:54 +0300

    Update code.js

--- code.js
+++ code.js
@@ -1,13 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   const apiKey = 'f2ee48b1843341f083762954250802';
 
+  const searchText = document.getElementById('searchText');
+  const buttonSearch = document.getElementById('buttonSearch');
+  const tableData = document.getElementById('tableData');
+
   const fetchData = async (cityName) => {
     const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${cityName}&aqi=no&lang=ru`);
     const responseJSON = await response.json();
     return responseJSON;
   }
   
-  fetchData('Yaroslavl');
-  fetchData('Moscow');
-  fetchData('Sochi');
+  buttonSearch.addEventListener('click', () => {
+    
+  })
 });
