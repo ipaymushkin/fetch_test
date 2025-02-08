d1e859da71dea8232ea9360e9e2088ee3eed9f32
ipaymushkin <57440215+ipaymushkin@users.noreply.github.com>
2025-02-08 10:52:02 +0300

    Update code.js

--- code.js
+++ code.js
@@ -1,10 +1,16 @@
 document.addEventListener("DOMContentLoaded", async () => {
   const apiKey = 'f2ee48b1843341f083762954250802';
 
-  try {
-    const response = await fetch('http://api.weatherapi.com/v1/current.json?key=f2ee48b1843341f083762954250802&q=London&aqi=no');
-    console.log(response);
-  } catch (error) {
-    console.log(error)
+  const fetchData = (cityName) => {
+    try {
+      const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${cityName}&aqi=no&lang=ru`);
+      console.log(response);
+    } catch (error) {
+      console.log(error)
+    }
   }
+
+  fetchData('Yaroslavl');
+  fetchData('Moscow');
+  fetchData('Sochi');
 });
